b412644a16b5c2721dec990852d80e1f65009e40
Sam Davenport <samd@carolina.rr.com>
2025-06-03 17:37:22 -0400

    Initial Commit

---
+++ CP2077 Save Manager/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Hello World!! This is the CP2077 Save Manager!!!");
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Nice to meet you " + name);
+        Console.WriteLine("Have a good day!!!!");
+        return;
+    }
+}
