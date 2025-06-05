e70f06c449a15a962a022c41233a8ed368f82a39
Sam Davenport <samd@carolina.rr.com>
2025-06-04 23:50:36 -0400

    Save object now stores both voice and body gender seperately, small refactor in Program + starting to implement text prompts, added documentation.

--- CP2077 Save Manager/Program.cs
+++ CP2077 Save Manager/Program.cs
@@ -9,58 +9,48 @@ namespace saveManager
 {
     class Program
     {
-        static string savesDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\Saved Games\\CD Projekt Red\\Cyberpunk 2077";
+        static string savesDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Saved Games\\CD Projekt Red\\Cyberpunk 2077"; //CP2077 save file location in Windows 11
 
         static void Main()
         {
-            List<string> savesList;
+            List<string> savesDirList;
 
-            Console.WriteLine("Hello World!! This is the CP2077 Save Manager!!!");
+            Console.WriteLine("Welcome to the CP2077 Save Manager!!!");
+            Console.WriteLine("Scanning your save file directory...");
 
-            savesList = new List<string>(Directory.EnumerateDirectories(savesDir));
+            savesDirList = new List<string>(Directory.EnumerateDirectories(savesDir)); //put the paths to all save directories in a list
 
-            Save[] allSavesArr = new Save[savesList.Count];
+            Console.WriteLine("Directory scan complete. Building save file list...");
 
-            foreach (var (index, item) in savesList.Select((item, index) => (index, item))) 
+            Save[] allSaves = new Save[savesDirList.Count];
+            List<string> tempTypeList = new List<string>();
+
+            foreach (var (index, item) in savesDirList.Select((item, index) => (index, item))) //deserialize each save file's metadata and use it to build array of all Saves
             {
-                
-                string fileName = item+"\\metadata.9.json";
+                string fileName = item + "\\metadata.9.json";
                 string jsonString = File.ReadAllText(fileName);
                 JsonDocument document = JsonDocument.Parse(jsonString);
                 JsonElement root = document.RootElement.GetProperty("Data").GetProperty("metadata");
 
-                Save save = new Save (root.GetProperty("name").GetString(),
-                                    item,
-                                    (LifePath)Enum.Parse(typeof(LifePath),root.GetProperty("lifePath").GetString()),
-                                    (Gender)Enum.Parse(typeof(Gender),root.GetProperty("bodyGender").GetString())
+                allSaves[index] = new Save(
+                                        root.GetProperty("name").GetString(),
+                                        item,
+                                        (LifePath)Enum.Parse(typeof(LifePath), root.GetProperty("lifePath").GetString()),
+                                        (BodyGender)Enum.Parse(typeof(BodyGender), root.GetProperty("bodyGender").GetString()),
+                                        (VoiceGender)Enum.Parse(typeof(VoiceGender), root.GetProperty("brainGender").GetString())
                                     );
 
-                allSavesArr[index] = save;
+                tempTypeList.Add($"{allSaves[index].lifePath} ({allSaves[index].bodyGender} Body + {allSaves[index].voiceGender} Voice)"); //populate temp list of type strings to then build final list for UI
 
-                Console.WriteLine(allSavesArr[index].saveName + " - " + allSavesArr[index].charGender + " " + allSavesArr[index].charPath);
+                //Console.WriteLine(allSaves[index].saveName + " - " + allSaves[index].charType);
             }
 
-            /* string fileName = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\Saved Games\\CD Projekt Red\\Cyberpunk 2077\\QuickSave-7\\metadata.9.json";
-            string jsonString = File.ReadAllText(fileName);
-            JsonDocument document = JsonDocument.Parse(jsonString);
+            List<string> saveTypeList = tempTypeList.Distinct().ToList();
+            //saveTypeList.ForEach(Console.WriteLine);
 
-            JsonElement root = document.RootElement.GetProperty("Data").GetProperty("metadata");
+        }
 
-            Console.WriteLine(root.GetProperty("lifePath").GetRawText());
-            Console.WriteLine(root.GetProperty("bodyGender").GetRawText());
-            Console.WriteLine(root.GetProperty("name").GetRawText());
-            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
 
-            testSave = new Save (root.GetProperty("name").GetRawText(),
-                                 Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\Saved Games\\CD Projekt Red\\Cyberpunk 2077\\QuickSave-7",
-                                 (LifePath)Enum.Parse(typeof(LifePath),root.GetProperty("lifePath").GetString()),
-                                 (Gender)Enum.Parse(typeof(Gender),root.GetProperty("bodyGender").GetString())
-                                );
-
-            Console.WriteLine("Save Name: "+testSave.saveName);
-            Console.WriteLine("Lifepath: "+testSave.charPath);
-            Console.WriteLine("Gender: "+testSave.charGender); */
-        }   
     }
 
 }
--- CP2077 Save Manager/Save.cs
+++ CP2077 Save Manager/Save.cs
@@ -3,25 +3,28 @@ using System;
 namespace saveManager
 {
     public enum LifePath {StreetKid,Nomad,Corpo}
-    public enum Gender {Male,Female}
+    public enum BodyGender {Male,Female}
+    public enum VoiceGender {Male,Female}
 
     public class Save
     {
 
         public string saveName { get; set; }
         public string saveDir { get; set; }
-        public LifePath charPath { get; set; }
-        public Gender charGender { get; set; }
+        public LifePath lifePath { get; set; } //Nomad, StreetKid, or Corporate
+        public BodyGender bodyGender { get; set; } //Body type, either male or female 
+        public VoiceGender voiceGender { get; set; }
 
-        public Save(string name, string dir, LifePath path, Gender gender) 
-        { 
-            saveName=name;
-            saveDir=dir;
-            charPath=path;
-            charGender=gender;
+        public Save(string name, string dir, LifePath path, BodyGender bod, VoiceGender vox)
+        {
+            saveName = name;
+            saveDir = dir;
+            lifePath = path;
+            bodyGender = bod;
+            voiceGender = vox;
         }
 
-        public Save() {}
-        
+        public Save() { }
+
     }
 }
